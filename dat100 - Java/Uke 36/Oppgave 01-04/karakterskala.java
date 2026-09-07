1173a2f857d56f9a4980f6e68d1e4ebb92b6a27e
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-07 22:42:57 +0200

    skole: automatisk synk 2026-09-07 22:42

--- dat100 - Java/Uke 36/Oppgave 01-04/karakterskala.java
+++ dat100 - Java/Uke 36/Oppgave 01-04/karakterskala.java
@@ -6,23 +6,34 @@ public class karakterskala {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Skriv poeng (0-100): ");
-        int poeng = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
 
-        if (poeng < 0 || poeng > 100) {
-            System.out.println("ugyldig poengsum.");
-        } else if (poeng >= 89) {
-            System.out.println("Karakter: A");
-        } else if (poeng >= 77) {
-            System.out.println("Karakter: B");
-        } else if (poeng >= 65) {
-            System.out.println("Karakter: C");
-        } else if (poeng >= 53) {
-            System.out.println("Karakter: D");
-        } else if (poeng >= 41) {
-            System.out.println("Karakter: E");
-        } else {
-            System.out.println("Karakter: F");
+            int poeng;
+
+            // Les inn på nytt til poengsummen er gyldig
+            do {
+                System.out.print("Student " + i + " - poengsum (0-100): ");
+                poeng = input.nextInt();
+
+                if (poeng < 0 || poeng > 100) {
+                    System.out.println("Ugyldig poengsum, prøv igjen.");
+                }
+            } while (poeng < 0 || poeng > 100);
+
+            // Her er poeng garantert mellom 0 og 100
+            if (poeng >= 89) {
+                System.out.println("Student " + i + " får karakter A");
+            } else if (poeng >= 77) {
+                System.out.println("Student " + i + " får karakter B");
+            } else if (poeng >= 65) {
+                System.out.println("Student " + i + " får karakter C");
+            } else if (poeng >= 53) {
+                System.out.println("Student " + i + " får karakter D");
+            } else if (poeng >= 41) {
+                System.out.println("Student " + i + " får karakter E");
+            } else {
+                System.out.println("Student " + i + " får karakter F");
+            }
         }
 
         input.close();

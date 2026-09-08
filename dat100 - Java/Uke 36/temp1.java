80ec0fea9f15b5b5fdcc3c5da6775acb5936db23
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-08 21:05:08 +0200

    skole: automatisk synk 2026-09-08 21:05

---
+++ dat100 - Java/Uke 36/temp1.java
@@ -0,0 +1,17 @@
+public class temp1 {
+    public static void main(String[] args) {
+        
+        int[] temp = {12, 15, 9, 21, 18, 7};
+    
+        for (int i : temp) {
+            System.out.print("[ ");
+            
+            for (int x : temp) {
+            System.out.print(x);
+        }
+        }
+    
+        
+        
+    }
+}

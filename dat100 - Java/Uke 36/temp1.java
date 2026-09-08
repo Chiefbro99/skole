e58e7245792d073e4d8c76e0517d3e1e58a503d4
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-08 21:22:47 +0200

    skole: automatisk synk 2026-09-08 21:22

--- dat100 - Java/Uke 36/temp1.java
+++ dat100 - Java/Uke 36/temp1.java
@@ -1,17 +1,33 @@
+
 public class temp1 {
     public static void main(String[] args) {
         
         int[] temp = {12, 15, 9, 21, 18, 7};
-    
-        for (int i : temp) {
-            System.out.print("[ ");
-            
-            for (int x : temp) {
-            System.out.print(x);
+        System.out.println("Oppgave1");
+        System.out.print("[ ");
+
+        for (int x : temp) {
+        System.out.print(x + ", ");
         }
-        }
-    
+        System.out.println("]");
+        
+        System.out.println("");
         
         
+        //Tvinge alt inn i forløkke:
+        for (int i = 0; i < temp.length; i++) {
+            if (i == 0) System.out.print("[ ");
+        System.out.print(temp[i] + ", ");
+            if (i == temp.length - 1) System.out.print("]");
+        }
+        System.out.println();
+        
+        System.out.println("Oppgave2");
+        
+        //Skriv om fra celcius til farenheit.
+        int f = c * 9 / 5 + 32;
+        for (int f < temp.length) {
+            System.out.print("test");
+        }
     }
 }

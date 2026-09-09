9bd3f4a7d6c43fb9323bf0b4725f5f00eb91ad8f
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-09 10:34:08 +0200

    skole: automatisk synk 2026-09-09 10:34

---
+++ dat100 - Java/Uke 36/treningsloggtest.java
@@ -0,0 +1,14 @@
+public class treningsloggtest {
+    public static void main(String[] args) {
+        private static final int MAKS_OKTER = 20; //Setter maks antall økter
+        private static final int SOYLE_BREDDE = 30; //Setter maks bredde på søyler
+        
+        
+            
+                
+                    
+                
+            
+        
+    }
+}

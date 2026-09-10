604e53497a93cc5247aa24a928fdf5029c6f47ae
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-10 11:33:05 +0200

    skole: automatisk synk 2026-09-10 11:33

--- dat100 - Java/Uke 36/smott/prove.java
+++ dat100 - Java/Uke 36/smott/prove.java
@@ -1,5 +1,22 @@
+//Obligatorisk skoleprøve i DAT100 - UKE 37 DEL2
 public class prove {
     public static void main(String[] args) {
-        System.out.println("Hei fra prove!");
+        //Oppgave 1 (vekt 20%)
+        //a)
+        int a = 7;
+        if (a <= 0 || a >= 0) {
+            System.out.println(a);
+        }
+        
+        
+        //b)
+        if (a >= 5 && a <= 8) {
+            System.out.print(a + " er innenfor intervallet");
+        } else {
+            System.out.print(a + " er utenfor intervallet");
+        }
+        
+        
+        
     }
 }

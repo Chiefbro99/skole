ce9aa2087b62a64879ca57e3075063af3ac68d9f
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-20 12:49:02 +0200

    skole: automatisk synk 2026-09-20 12:49

--- dat100 - Java/Uke 39/Øving/Datatabell.java
+++ dat100 - Java/Uke 39/Øving/Datatabell.java
@@ -1,17 +1,36 @@
+/*
+ * Datatabell.java
+ *
+ * Viser forskjellen på å endre primitive verdier og objekter i en for-each-løkke.
+ */
+
 public class Datatabell {
+
     public static void main(String[] args) {
-        Data[] tab = new Data[3];
-        
-        tab[0] = new Data(1);
-        tab[1] = new Data(2);
-        tab[2] = new Data(3);
-        
+
+        // Arrayet lages med tre TOMME plasser - alle inneholder null nå.
+        Data[] tab = new Data[10];
+
+        // Her fyller vi plassene med faktiske objekter.
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = new Data(i + 1);
+        }
+
+        // Skriv ut før endring.
+        // System.out.print(d) kaller automatisk d.toString() for å få tekst.
         for (Data d : tab) {
             System.out.print(d + " ");
         }
-        
-        System.out.println();
-        
+        System.out.println();   // uten argument = bare linjeskift
+
+        /*
+         * Oppdater. Dette VIRKER, i motsetning til x = x + 1 på en int-tabell.
+         *
+         * d inneholder en kopi av ADRESSEN til objektet, og den adressen
+         * peker på samme objekt som tab[i] gjør. Når vi kaller setData(),
+         * følger vi adressen fram og endrer objektet der det ligger.
+         * Arrayet ser endringen fordi det er ett og samme objekt.
+         */
         for (Data d : tab) {
             d.setData(d.getData() + 1);
         }
@@ -21,26 +40,41 @@ public class Datatabell {
             System.out.print(d + " ");
         }
         System.out.println();
-        }
     }
-    
-    class Data {
-        private int data;
-        
-        public Data(int data) {
-            this.data = data;
-        }
-        
-        public int getData() {
-            return data;
-        }
-        
-        public void setData(int data) {
-            this.data = data;
-        }
-        
-        @Override
-        public String toString() {
-            return "" + data;
-        }
+}
+
+
+/*
+ * Egen klasse på toppnivå - utenfor Datatabell sine krøllparenteser.
+ * Ikke public, fordi bare klassen som matcher filnavnet kan være det.
+ */
+class Data {
+
+    // private: bare Data selv kan røre feltet direkte.
+    private int data;
+
+    public Data(int data) {
+        this.data = data;   // this.data er feltet, data er parameteren
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    /*
+     * toString() finnes allerede i Object, som alle klasser arver fra.
+     * Standardversjonen gir "Data@1b6d3586" - klassenavn og minneadresse.
+     * Ved å overstyre den bestemmer vi selv hvordan objektet ser ut som tekst.
+     *
+     * "" + data limer tallet på en tom streng, som er en kortform for
+     * å konvertere int til String.
+     */
+    @Override
+    public String toString() {
+        return "" + data;
+    }
+}

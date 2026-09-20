558f093eb1cb902b6db5d18ac43f80ef280c8cd8
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-20 16:22:07 +0200

    skole: automatisk synk 2026-09-20 16:22

--- dat100 - Java/Uke 36/Uke 38/OppgaveG.java
+++ dat100 - Java/Uke 36/Uke 38/OppgaveG.java
@@ -1,9 +1,28 @@
-/* Denne koden lager en tabell med heltall*/
+/* Denne koden lager en tabell med heltall og skriver det ut på 2 måter*/
 import java.util.Arrays;
 
 public class OppgaveG {
     public static void main(String[] args) {
+        //oppgave a
         int[] tabell = {1,3,4,7,9,10};
-        System.out.print(Arrays.toString((tabell));
+        //Utskrift1
+        System.out.println(Arrays.toString(tabell));
+        
+        //utskrift2
+        for (int x : tabell) {
+            System.out.print(x + " ");
+        }
+       
+        System.out.println();
+       
+       //Oppgave b
+       System.out.println(tabell[2]);   // 4
+       
+       //Oppgave c
+       tabell[2] = 7;
+       System.out.println(tabell[2]);
+       
     }
 }
+
+

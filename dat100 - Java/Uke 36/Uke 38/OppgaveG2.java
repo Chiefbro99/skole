35d2d209fc842d71874a3566281a450a6018f27d
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-20 17:28:27 +0200

    skole: automatisk synk 2026-09-20 17:28

--- dat100 - Java/Uke 36/Uke 38/OppgaveG2.java
+++ dat100 - Java/Uke 36/Uke 38/OppgaveG2.java
@@ -1,5 +1,24 @@
 public class OppgaveG2 {
     public static void main(String[] args) {
+        String breddeStr = showInputDialog("Bredde:");
+        int bredde = parseInt(breddeStr);
+
+        String hoydeStr = showInputDialog("Høyde:");
+        int hoyde = parseInt(hoydeStr);
+
+        int a = areal(bredde,hoyde);
+
+        showMessageDialog(null,"Areal: " + a);
+    }
+    
+    private static int areal(int bredde, int hoyde) {
+
+        int flateareal = bredde * hoyde;
+
+        return flateareal;
+    }
+    
+    private static int lesInnTall(String message) {
         
     }
 }

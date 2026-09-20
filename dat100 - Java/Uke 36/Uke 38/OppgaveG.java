b9c3f4944db845620dabc0d993d6726e5c0d9c82
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-20 16:04:31 +0200

    skole: automatisk synk 2026-09-20 16:04

---
+++ dat100 - Java/Uke 36/Uke 38/OppgaveG.java
@@ -0,0 +1,9 @@
+/* Denne koden lager en tabell med heltall*/
+import java.util.Arrays;
+
+public class OppgaveG {
+    public static void main(String[] args) {
+        int[] tabell = {1,3,4,7,9,10};
+        System.out.print(Arrays.toString((tabell));
+    }
+}

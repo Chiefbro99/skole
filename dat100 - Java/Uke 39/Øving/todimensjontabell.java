8d80a33f68907a5c286ecfd2dfad770eacdcbdb1
Lexi skole-synk <lexi@raspberrypi-1.local>
2026-09-20 14:25:51 +0200

    skole: automatisk synk 2026-09-20 14:25

---
+++ dat100 - Java/Uke 39/Øving/todimensjontabell.java
@@ -0,0 +1,9 @@
+public class todimensjontabell {
+    public static void main(String[] args) {
+        int Antall = 8;
+        int Rader = 6;
+        
+        boolean [][] f118 = new boolean [Rader][Antall];
+        System.out.println(f118);
+    }
+}
